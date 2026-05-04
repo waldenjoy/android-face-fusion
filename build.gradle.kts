4ba304e8d3b9d7a5fd258973d4fb510572ee7587
waldenjoy <144027732+waldenjoy@users.noreply.github.com>
2026-05-04 18:36:08 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-android {
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isShrinkResources = false
-
-            // IMPORTANT: makes release APK installable for testing
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
 }
